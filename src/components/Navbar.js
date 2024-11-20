07c505b016cab2249a1e53e19fe1483a0652949b
nancy-kataria <katarianancy8@gmail.com>
2024-11-19 23:41:40 -0800

    added signout button and changed code for organiser event handling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 // components/Navbar.js
-import Link from 'next/link';
-import { useAuth } from '@/context/AuthContext';
-
+import Link from "next/link";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar() {
-  const {user} = useAuth();
+  const { user } = useAuth();
   return (
     <nav className="flex items-center justify-between px-6 py-4 bg-blue-500 text-white shadow-md">
       {/* Logo */}
@@ -19,20 +18,24 @@ export default function Navbar() {
         </Link>
 
         {user ? (
-      <>
-      {user.userType === "Organizer" && (
-        <Link href="/createEvent" passHref>
-        <div className="cursor-pointer hover:underline">Create Event</div>
-      </Link>
-      )}
-      </>
-        ) :
-        (
-        <Link href="/login" passHref>
-          <div className="cursor-pointer hover:underline">Sign In</div>
-        </Link>
+          user.userType === "Organizer" && (
+            <Link href="/createEvent" passHref>
+              <div className="cursor-pointer hover:underline">Create Event</div>
+            </Link>
+          )
+        ) : (
+          <></>
         )}
-        
+
+        {user ? (
+          <>
+            <div className="cursor-pointer hover:underline">Sign Out</div>
+          </>
+        ) : (
+          <Link href="/login" passHref>
+            <div className="cursor-pointer hover:underline">Sign In</div>
+          </Link>
+        )}
       </div>
     </nav>
   );
