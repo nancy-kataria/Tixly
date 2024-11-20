7fa80ce8ff0919291fa2358ebd407dc3b1aad11b
nancy-kataria <katarianancy8@gmail.com>
2024-11-20 13:08:39 -0800

    added transferring ticket ownership api

--- src/app/api/ticketOwnership/update/[id]/route.js
+++ src/app/api/ticketOwnership/update/[id]/route.js
@@ -1,19 +1,34 @@
-import connectDB from '@/lib/mongooseDB';
-import Event from '@/models/Event';
-import Venue from '@/models/Venue';
-import TicketOwnership from '@/models/TicketOwnership';
+import connectDB from "@/lib/mongooseDB";
+import TicketOwnership from "@/models/TicketOwnership";
 
+export async function PATCH(request, { params }) {
+  const { id } = await params;
+  const data = await request.json();
 
-export async function POST(request, {params}) {
-    const {id} = await params;
-    const data = await request.json();
+  if (!data.userId) {
+    return new Response(
+      JSON.stringify({ error: "ID and userId are required" }),
+      { status: 400 }
+    );
+  }
+  try {
+    await connectDB();
+    // Find the ownership record and update the userID
+    const updatedRecord = await TicketOwnership.findByIdAndUpdate(
+      id,
+      { userID: data.userId }, // Update the userID field
+      { new: true, runValidators: true } // Return the updated document
+    );
 
-    try{
-        
-        await connectDB();
-        
+    if (!updatedRecord) {
+      return new Response(JSON.stringify({ error: "Record not found" }), {
+        status: 404,
+      });
     }
-    catch (error) {
-      return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-    }
+    return new Response(JSON.stringify({ data: updatedRecord }), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
 }
