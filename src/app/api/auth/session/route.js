2543ab124f415dc842e41ab6792e2b341fc86718
nancy-kataria <katarianancy8@gmail.com>
2024-11-19 22:30:41 -0800

    added comments to API

--- src/app/api/auth/session/route.js
+++ src/app/api/auth/session/route.js
@@ -1,14 +1,20 @@
 import { getServerSession } from "next-auth";
 
+// Next.js API route function
+// handles HTTP GET requests
 export async function GET(req) {
   try {
+    // getServerSession checks if the user is authenticated by verifying their session token, usually stored in cookies or headers.
     const session = await getServerSession(req);
 
     if (session) {
+      // If the user is authenticated, the response sends back the user information in JSON format with a status code of 200
       return new Response(
         JSON.stringify({ user: session.user }),
         { status: 200, headers: { "Content-Type": "application/json" } }
       );
+      // new Response() creates an HTTP response object that can be returned to the client making the request
+      // It is a part of the Web Fetch API
     } else {
       return new Response(
         JSON.stringify({ error: "Not authenticated" }),
