f6120220adb1e41d57b3eb4c274b964db3d8dfa0
nancy-kataria <katarianancy8@gmail.com>
2024-12-01 23:29:42 -0800

    API param correction

--- src/app/api/tickets/list/[id]/route.js
+++ src/app/api/tickets/list/[id]/route.js
@@ -1,25 +1,24 @@
 import connectDB from "@/lib/mongooseDB";
 import TicketOwnership from "@/models/TicketOwnership";
 
-export async function GET({params}) {
-  const { id } = await params;
-  console.log(id)
+export async function GET(req, {params} ) {
+    const {id} = await params;
 
-  if (!id) {
-    return new Response(
-      JSON.stringify({ error: "ID is required" }),
-      { status: 400 }
-    );
-  }
+    if (!id) {
+      return new Response(
+        JSON.stringify({ error: "ID is required" }),
+        { status: 400 }
+      );
+    }
 
-  try {
-    await connectDB();
-    const ticketList = await TicketOwnership.find({ userID: id });
+    try {
+      await connectDB();
+      const ticketList = await TicketOwnership.find({ userID: id });
 
-    return new Response(JSON.stringify({ data: ticketList }), { status: 201 });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-    });
-  }
+      return new Response(JSON.stringify({ data: ticketList }), { status: 201 });
+    } catch (error) {
+      return new Response(JSON.stringify({ error: error.message }), {
+        status: 500,
+      });
+    }
 }
