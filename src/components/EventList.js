60b7888cd3fd9b80b0547c602e9b58c5c9ffa141
nancy-kataria <katarianancy8@gmail.com>
2024-12-09 22:31:29 -0800

    event card rearrangement

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,56 +1,50 @@
-"use client"
-import  { useState, useEffect } from "react";
+"use client";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import concert from "../../public/concert.jpg";
 import Image from "next/image";
 
-export default function EventList({eventList})
-{
-    const router = useRouter();
-    if (!eventList || eventList.length === 0)
-    {
-        console.log("Eventlist is empty");
-        return <p className="text-sm font-medium">No events yet</p>;
-    }
-    
-    return (
-        <>
-        {/* Display search results */}
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {eventList.map((event) => (
-              <div
-                key={event._id}
-                onClick={()=>{router.push(`/event/${event._id}`)}}
-                className="w-full max-w-lg p-4 bg-white border border-gray-300 rounded-lg shadow-md flex items-center space-x-4"
-              >
-                {/* Event Image */}
-                <Image
-                  src={concert}
-                  alt="concert-image"
-                  className="w-24 h-24 rounded-lg object-cover"
-                />
+export default function EventList({ eventList }) {
+  const router = useRouter();
+  if (!eventList || eventList.length === 0) {
+    console.log("Eventlist is empty");
+    return <p className="text-sm font-medium">No events yet</p>;
+  }
 
-                {/* Event Details */}
-                <div>
-                  {/* Event Name */}
-                  <h2 className="text-2xl font-bold text-gray-800">
-                    {event.eventName}
-                  </h2>
+  return (
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+      {eventList.map((event) => (
+        <div
+          key={event._id}
+          onClick={() => {
+            router.push(`/event/${event._id}`);
+          }}
+          className="w-full max-w-lg p-4 bg-white border border-gray-300 rounded-lg shadow-md items-center space-x-4"
+        >
+          {/* Event Image */}
+          <Image
+            src={concert}
+            alt="concert-image"
+            className="w-100 h-100 rounded-lg object-cover"
+          />
 
-                  {/* Date and Venue */}
-                  <div className="text-gray-600 mt-2">
-                    <p className="text-sm font-medium">
-                      📅 Date: ({new Date(event.eventDate).toLocaleDateString()}
-                      )
-                    </p>
-                    <p className="text-sm font-medium">
-                      📍 Venue: {event.venue}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
+          {/* Event Details */}
+          <div>
+            {/* Event Name */}
+            <h2 className="text-2xl font-bold text-gray-800">
+              {event.eventName}
+            </h2>
+
+            {/* Date and Venue */}
+            <div className="text-gray-600 mt-2">
+              <p className="text-md font-medium">
+                📅 Date: {new Date(event.eventDate).toLocaleDateString()}
+              </p>
+              <p className="text-md font-medium">📍 Venue: {event.venue}</p>
+            </div>
           </div>
-      </>
-    )
+        </div>
+      ))}
+    </div>
+  );
 }
