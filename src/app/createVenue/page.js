6db06a2197fd0ce5b55962a0e8e168a93bb3766c
nancy-kataria <katarianancy8@gmail.com>
2024-12-09 23:52:58 -0800

    fixed client side errors

--- src/app/createVenue/page.js
+++ src/app/createVenue/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from "react";
 
 function page() {
@@ -53,7 +53,7 @@ function page() {
             onChange={handleFormDataChange}
             required
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-400"
-            placeholder="Enter venue name"
+            placeholder="Enter Venue Name"
           />
         </div>
 
