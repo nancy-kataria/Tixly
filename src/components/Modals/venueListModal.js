37bba2c938a569eb6b35944ca7ff8390a1375c07
nancy-kataria <katarianancy8@gmail.com>
2024-12-12 23:27:18 -0800

    made modal scrollable

--- src/components/Modals/venueListModal.js
+++ src/components/Modals/venueListModal.js
@@ -9,7 +9,7 @@ function VenueListModal({
 }) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-lg p-6 max-w-sm w-full text-gray-800">
+      <div className="bg-white rounded-lg shadow-lg p-6 max-w-sm w-full text-gray-800 h-100 overflow-y-scroll">
         <h2 className="text-xl font-semibold mb-4">
           Select a Venue for your event
         </h2>
@@ -28,7 +28,7 @@ function VenueListModal({
           >
             {/* venue Details */}
             <div>
-              <h2 className="text-2xl font-bold text-gray-800">{venue.name}</h2>
+              <h2 className="text-lg font-bold text-gray-800">{venue.name}</h2>
               <div className="text-gray-600 mt-2">
                 <p className="text-sm font-medium">
                   📍 Address: {venue.address}
